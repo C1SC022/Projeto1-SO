9c07e0aad707e5e9e8046fedab43f5a0ea3751c8
LeTo <tomascostag@gmail.com>
2024-10-22 14:57:44 +0100

    check backup folder function and some minor changes

--- backup_files.sh
+++ backup_files.sh
@@ -6,7 +6,13 @@ function main(){
 
     check_arg_amt "$@"
     check_arg_path
-    do_backup
+    if check_backup_existence ; 
+    then
+        echo "o check funciona"
+        do_initial_backup
+    else
+        echo "o backup existe"
+    fi
 }
 
 
@@ -15,7 +21,8 @@ function main(){
 function check_arg_amt()
 {
 
-    if [ $# != 2 ]; then
+    if [ $# != 2 ]; 
+    then
         echo "The number of arguments is wrong"
         exit 1
     fi
@@ -23,22 +30,31 @@ function check_arg_amt()
 }
 
 
-
-
 #checkar se os diretórios dados sao válidos
 function check_arg_path()
 {
 
-    if [ ! -d "$src_dir" ] || [ ! -d "$dst_dir" ]; then 
+    if [ ! -d "$src_dir" ] || [ ! -d "$dst_dir" ]; 
+    then 
          echo "The directories inputed do not exist!"
          exit 1
     fi
 
 }
 
+function check_backup_existence()
+{
+
+    if [ -d "$dst_dir/backup" ];
+    then
+        return 1;
+    fi
+
+    return 0;
+}
 
 
-function do_backup()
+function do_initial_backup()
 {
 
     new_folder="$dst_dir/backup"
@@ -50,18 +66,17 @@ function do_backup()
 }
 
 
-
-
-function compareData()
+function compare_data()
 {
     file1=$1
     file2=$2
 
-    if [ "$file1" -nt "$file2" ]; then
+    if [ "$file1" -nt "$file2" ]; 
+    then
         return 0;
-    else
-        return 1;
     fi
+
+    return 1;
 }
 
 function compare()
@@ -71,16 +86,18 @@ function compare()
     #analisar files de fonte->backup
     for file in "$src_dir"/*; do
         file_name=$(basename "$file")
-        file1="$src_dir/$file_name"
+        src_file="$src_dir/$file_name"
         
 
-        if [[ -f "$dst_dir/$file_name" ]]; then
-            file2="$dst_dir/$file_name"
-            if compareData "$file1" "$file2" ; then #executa quando returnar 0
-                cp "$file1" "$file2" #substitui o ficheiro 2 com o 1
+        if [ -f "$dst_dir/$file_name" ]; 
+        then
+            dst_file="$dst_dir/$file_name"
+            if compare_data "$src_file" "$dst_file" ; 
+            then #executa quando retornar 0
+                cp "$src_file" "$dst_file" #substitui o ficheiro 2 com o 1
             fi
         else
-            cp "$file1" "$dst_dir"
+            cp "$src_file" "$dst_dir"
         fi
 
     done
