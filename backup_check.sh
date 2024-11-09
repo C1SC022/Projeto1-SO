8e8e2b19601903a5b307735b13a7c83be0fba2a7
LeTo <tomascostag@gmail.com>
2024-11-09 17:05:36 +0000

    Backup_check.sh está completo e bué readable e todo comentado :3

--- backup_check.sh
+++ backup_check.sh
@@ -1,18 +1,19 @@
+# Declaration of global variables
 src_dir="$1"
 dst_dir="$2"
 
 function main()
 {
+    # Check if the backup directory indicated exists
     if  check_backup_existence;
     then
         echo -e "\033[31mA backup folder still doesn't exist in that directory.\033[0m"
-        end
+        exit # Exit's the script
     else
         echo -e "The backup folder exists in the \033[33m$dst_dir\033[0m directory."
         dst_dir="$dst_dir/backup"
-        check_backup_differences
     fi
-    
+    check_backup_differences
 }
 
 
@@ -21,28 +22,30 @@ function check_backup_existence()
 
     if [ -d "$dst_dir/backup" ];
     then
-        return 1;
+        return 1; # Backup directory exists return true (1)
     fi
 
-    return 0;
+    return 0; # Doesn't exist return false (0)
 }
 
 function check_backup_differences()
 {
+    # Indicate the start of the comparison
     echo -e "\033[32mBeginning comparison...\033[0m"
 
+    # Loop through the source files and check if they exist in the backup directory
     find "$src_dir" -type f | while IFS= read -r src_file; do
         rel_path="${src_file#$src_dir/}"
         b_file="$dst_dir/$rel_path"
         #echo "$rel_path"
 
         # Check if the file exists in the backup
-        if ! check_file_existence "$b_file"; then
+        if  check_file_existence "$b_file"; then
             echo "$filename exists in source but not in backup"
             continue
         fi
 
-        # Compare MD5 checksums of the source and backup file
+        # Compare md5sum's of the source and backup file
         src_md5=$(md5sum "$src_file" | awk '{print $1}')
         b_md5=$(md5sum "$b_file" | awk '{print $1}')
 
@@ -51,31 +54,33 @@ function check_backup_differences()
         fi
     done
 
-    # Loop through backup files and check if they exist in source
+    # Loop through the backup files and check if they exist in the source directory
     find "$dst_dir" -type f | while IFS= read -r b_file; do
         rel_path="${b_file#$dst_dir/}"
         src_file="$dst_dir/$rel_path"
         #echo "$rel_path"
 
-        if ! check_file_existence "$src_file"; then
+        # Check if the file exists in the source
+        if  check_file_existence "$src_file"; then
             echo "$filename exists in backup but not in source."
         fi
     done
 
+    # Indicate completion of the comparison
     echo -e "\033[32mComparison complete\033[0m"
 
 }
 
 function check_file_existence()
 {
-    if [ ! -f "$1" ];
+    if [ -f "$1" ];
     then
-        return 1
-    else 
-        return 0
-    fi
+        return 1 # file exists return true (1)
+    fi 
+    
+    return 0 # it doesn't exist return false (0)
 }
 
 
 
-main "$@"
+main "$@" # Initiate main
